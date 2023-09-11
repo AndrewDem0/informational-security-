c0f99c41dcb0865db327b4dd840eb3177f2fa74b
Andrew <Andrew@DESKTOP-T4IC0L8>
2023-09-11 20:38:11 +0300

    Add project files.

---
+++ Lab1.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class RandomNumber
+{
+    public static byte[] GenerateRandomNumber(int length)
+    {
+        using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+        {
+            var randomNumber = new byte[length];
+            randomNumberGenerator.GetBytes(randomNumber);
+            return randomNumber;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            byte[] randomBytes = RandomNumber.GenerateRandomNumber(32);
+            string randomNumber = Convert.ToBase64String(randomBytes);
+            Console.WriteLine(randomNumber);
+        }
+
+        Console.ReadLine();
+    }
+}
