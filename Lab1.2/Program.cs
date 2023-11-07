e5419022f19db125cfd6df11166b262de5511c8b
Andrij Demudenko <demedp.s@gmail.com>
2023-11-07 10:35:14 +0200

    створив 2 лаб без дешифрування методом грубої сили

--- Lab1.2/Program.cs
+++ Lab1.2/Program.cs
@@ -1,31 +1,63 @@
 ﻿using System;
-using System.Security.Cryptography;
+using System.IO;
 using System.Text;
-
-public class RandomNumber
-{
-    public static byte[] GenerateRandomNumber(int length)
-    {
-        using (var randomNumberGenerator = new RNGCryptoServiceProvider())
-        {
-            var randomNumber = new byte[length];
-            randomNumberGenerator.GetBytes(randomNumber);
-            return randomNumber;
-        }
-    }
-}
 
 class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i < 10; i++)
+        // Зчитування вмісту файлу "input.txt"
+        byte[] fileData = File.ReadAllBytes("input.txt");
+
+        // Генерування ключа (ключ повинен бути випадковим та тієї ж довжини, що і файл)
+        Random random = new Random();
+        byte[] key = new byte[8];
+        random.NextBytes(key);
+
+        // Шифрування даних і запис зашифрованих даних у файл "encrypted.dat"
+        byte[] encryptedData = new byte[8];
+        for (int i = 0; i < 8; i++)
         {
-            byte[] randomBytes = RandomNumber.GenerateRandomNumber(32);
-            string randomNumber = Convert.ToBase64String(randomBytes);
-            Console.WriteLine(randomNumber);
+            encryptedData[i] = (byte)(fileData[i] ^ key[i]);
+        }
+        File.WriteAllBytes("encrypted.dat", encryptedData);
+
+        // Розшифрування зашифрованих даних
+        byte[] decryptedData = new byte[encryptedData.Length];
+        for (int i = 0; i < encryptedData.Length; i++)
+        {
+            decryptedData[i] = (byte)(encryptedData[i] ^ key[i]);
+        }
+        File.WriteAllBytes("decrypted.txt", decryptedData);
+
+        // Дешифрування методом "грубої сили" та запис у файл "brute_force_decrypted.txt"
+        string bruteForceDecryptedText = null;
+
+
+        for (byte bruteForceKey = 0; bruteForceKey <= 255; bruteForceKey++)
+        {
+            byte[] bruteForceDecryptedData = new byte[8];
+            for (int i = 0; i < 8; i++)
+            {
+                bruteForceDecryptedData[i] = (byte)(encryptedData[i] ^ bruteForceKey);
+            }
+
+            string candidateText = Encoding.UTF8.GetString(bruteForceDecryptedData);
+            if (candidateText.Length == 8 && candidateText.StartsWith("Mit21"))
+            {
+                bruteForceDecryptedText = candidateText;
+                break; // Завершуємо, якщо знайдено відповідний ключ
+            }
         }
 
-        Console.ReadLine();
+        if (bruteForceDecryptedText != null)
+        {
+            File.WriteAllText("brute_force_decrypted.txt", bruteForceDecryptedText);
+            Console.WriteLine("Знайдено правильний ключ (методом грубої сили)");
+        }
+        else
+        {
+            Console.WriteLine("Не вдалося знайти правильний ключ методом грубої сили.");
+        }
     }
 }
