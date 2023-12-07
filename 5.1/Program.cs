843c3a7913bc3fc6c6b18231eb16c4b1303cfdb4
Andrij Demudenko <demedp.s@gmail.com>
2023-12-07 02:55:25 +0200

    закінчив 5 лабораторну

---
+++ 5.1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace _5._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Створюємо екземпляр класу SaltedHash для роботи з хешем та сіллю
+            SaltedHash passwords = new SaltedHash();
+
+            Console.Write("Введіть повідомлення для реалізації операції: ");
+            string message = Console.ReadLine();
+            // Отримуємо введений рядок та конвертуємо його в масив байтів
+            byte[] password = Encoding.Unicode.GetBytes(message);
+
+            // Викликаємо метод GetHash для отримання хешу та солі
+            passwords.GetHash(password);
+        }
+
+        public class SaltedHash
+        {
+            private byte[] Salt; // Змінна для зберігання солі
+            private byte[] SaltHashPassword; // Змінна для зберігання хешу пароля
+
+            // Метод для отримання хешу та солі
+            public void GetHash(byte[] Password)
+            {
+                // Створюємо генератор випадкових чисел
+                var randomNumberGenerator = new RNGCryptoServiceProvider();
+
+                // Генеруємо випадкову сіль довжиною 32 байти
+                Salt = new byte[32];
+                randomNumberGenerator.GetBytes(Salt);
+
+                // Об'єднуємо сіль та пароль в один масив байтів
+                var concatenatedData = new byte[Salt.Length + Password.Length];
+                //Buffer.BlockCopy(Salt, 0, concatenatedData, 0, Salt.Length);
+                Buffer.BlockCopy(Password, 0, concatenatedData, Salt.Length, Password.Length);
+
+                // Використовуємо SHA256 для обчислення хешу
+                using (var sha256 = SHA256.Create())
+                {
+                    SaltHashPassword = sha256.ComputeHash(concatenatedData);
+                }
+
+                // Виводимо сіль та хеш на консоль
+                Console.WriteLine("Сіль: " + Convert.ToBase64String(Salt));
+                Console.WriteLine("Хеш з сіллю: " + Convert.ToBase64String(SaltHashPassword));
+            }
+        }
+    }
+}
---
+++ 5.2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace _5._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Введення пароля від користувача
+            Console.WriteLine("Enter password");
+            string userPassword = Console.ReadLine();
+
+            // Запускаємо цикл для виконання хешування з різною кількістю ітерацій
+            for (int i = 70000; i < 70000 + (10 * 50000); i += 50000)
+            {
+                // Викликаємо метод для генерації хешу з використанням PBKDF2
+                HashAndPrint(userPassword, i);
+            }
+            Console.ReadLine();
+        }
+
+        public class PBKDF2
+        {
+            // Метод для генерації випадкової солі
+            public static byte[] GenerateSalt()
+            {
+                using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+                {
+                    var saltBytes = new byte[32];
+                    randomNumberGenerator.GetBytes(saltBytes);
+                    return saltBytes;
+                }
+            }
+
+            // Метод для хешування пароля з використанням PBKDF2
+            public static byte[] HashPassword(byte[] passwordBytes, byte[] salt, int i)
+            {
+                using (var rfc2898 = new Rfc2898DeriveBytes(passwordBytes, salt, i))
+                {
+                    return rfc2898.GetBytes(20);
+                }
+            }
+        }
+
+        // Метод для виведення результатів хешування на консоль
+        private static void HashAndPrint(string password, int i)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // Генеруємо сіль, хешуємо пароль та вимірюємо час виконання
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+            var salt = PBKDF2.GenerateSalt();
+            var hashedPassword = PBKDF2.HashPassword(passwordBytes, salt, i);
+
+            stopwatch.Stop();
+
+            // Виводимо результати на консоль
+            Console.WriteLine();
+            Console.WriteLine("Original Password: " + password);
+            Console.WriteLine("Hashed Password   : " + Convert.ToBase64String(hashedPassword));
+            Console.WriteLine("i <" + i + "> Elapsed Time: " + stopwatch.ElapsedMilliseconds + "ms");
+        }
+    }
+}
