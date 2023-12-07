c7519c45431b57723d6583e4e7c49be3c0973958
Andrij Demudenko <demedp.s@gmail.com>
2023-12-07 04:18:33 +0200

    6 лабораторна готова

---
+++ 6.1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    private static RSAParameters publicKey;
+    private static RSAParameters privateKey;
+
+    static void Main()
+    {
+        // Генеруємо та виводимо в консоль пару ключів
+        GenerateKeys();
+        Console.WriteLine("RSA Key Pair Generated...");
+        Console.WriteLine();
+        Console.WriteLine($"Public Key: {Convert.ToBase64String(publicKey.Modulus)}");
+        Console.WriteLine();
+        Console.WriteLine($"Private Key: {Convert.ToBase64String(privateKey.Modulus)}");
+        Console.WriteLine();
+
+        // Приклад вхідних даних для шифрування
+        string originalData = "Hello, RSA Encryption!";
+
+        // Шифруємо та виводимо результат
+        byte[] encryptedData = EncryptData(Encoding.UTF8.GetBytes(originalData));
+        Console.WriteLine("Encrypted Data: " + Convert.ToBase64String(encryptedData));
+        Console.WriteLine();
+
+        // Розшифровуємо та виводимо результат
+        byte[] decryptedData = DecryptData(encryptedData);
+        string decryptedString = Encoding.UTF8.GetString(decryptedData);
+        Console.WriteLine("Decrypted Data: " + decryptedString);
+    }
+
+    // Генерація пари ключів
+    static void GenerateKeys()
+    {
+        using (var rsa = new RSACryptoServiceProvider(2048))
+        {
+            publicKey = rsa.ExportParameters(false); // Експортуємо публічний ключ
+            privateKey = rsa.ExportParameters(true); // Експортуємо приватний ключ
+        }
+    }
+
+    // Шифрування даних
+    static byte[] EncryptData(byte[] dataToEncrypt)
+    {
+        byte[] cipherbytes;
+        using (var rsa = new RSACryptoServiceProvider())
+        {
+            rsa.PersistKeyInCsp = false; // Вимкнення збереження ключів в CSP
+            rsa.ImportParameters(publicKey); // Імпортуємо публічний ключ
+            cipherbytes = rsa.Encrypt(dataToEncrypt, true); // Шифруємо дані
+        }
+        return cipherbytes;
+    }
+
+    // Розшифрування даних
+    static byte[] DecryptData(byte[] dataToDecrypt)
+    {
+        byte[] plain;
+        using (var rsa = new RSACryptoServiceProvider())
+        {
+            rsa.PersistKeyInCsp = false; // Вимкнення збереження ключів в CSP
+            rsa.ImportParameters(privateKey); // Імпортуємо приватний ключ
+            plain = rsa.Decrypt(dataToDecrypt, true); // Розшифровуємо дані
+        }
+        return plain;
+    }
+}
---
+++ 6.2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    private const string PublicKeyFileName = "publicKey.xml";
+    private const string EncryptedMessageFileName = "encryptedMessage.txt";
+
+    private static RSAParameters _publicKey;
+
+    static void Main()
+    {
+        // Завантажити або згенерувати відкритий ключ
+        LoadOrGeneratePublicKey();
+
+        // Зберегти відкритий ключ у файл
+        SavePublicKeyToFile();
+
+        // Зашифрувати повідомлення
+        EncryptMessage("Hello, World!");
+
+        Console.WriteLine("Program executed successfully.");
+    }
+
+    private static void LoadOrGeneratePublicKey()
+    {
+        // Перевірити, чи існує файл із відкритим ключем
+        if (File.Exists(PublicKeyFileName))
+        {
+            // Якщо файл існує, завантажити відкритий ключ з файлу
+            _publicKey = LoadPublicKeyFromFile();
+        }
+        else
+        {
+            // Якщо файл не існує, згенерувати новий відкритий ключ
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                _publicKey = rsa.ExportParameters(false);
+            }
+        }
+    }
+
+    private static RSAParameters LoadPublicKeyFromFile()
+    {
+        // Завантажити відкритий ключ із файлу
+        using (var reader = new StreamReader(PublicKeyFileName))
+        {
+            var xmlString = reader.ReadToEnd();
+            var rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(xmlString);
+            return rsa.ExportParameters(false);
+        }
+    }
+
+    private static void SavePublicKeyToFile()
+    {
+        // Зберегти відкритий ключ у файл
+        using (var writer = new StreamWriter(PublicKeyFileName))
+        {
+            var rsa = new RSACryptoServiceProvider();
+            rsa.ImportParameters(_publicKey);
+            var xmlString = rsa.ToXmlString(false);
+            writer.Write(xmlString);
+        }
+    }
+
+    private static void EncryptMessage(string message)
+    {
+        // Зашифрувати повідомлення
+        using (var rsa = new RSACryptoServiceProvider())
+        {
+            rsa.ImportParameters(_publicKey);
+            var encryptedBytes = rsa.Encrypt(Encoding.UTF8.GetBytes(message), false);
+
+            // Зберегти зашифроване повідомлення у файл
+            File.WriteAllBytes(EncryptedMessageFileName, encryptedBytes);
+        }
+    }
+}
